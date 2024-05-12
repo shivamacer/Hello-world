1aad836efe93b57ff46d1cbdb4ab85856f695cca
shivam007 <shivamacer007@gmail.com>
2024-05-12 19:51:00 +0530

    initial commit

---
+++ Gift code/marks.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main(){
+
+int maths_marks;
+
+int science_marks;
+
+printf("Enter your maths marks: ");
+    
+    scanf("%d", &maths_marks);
+    
+	
+	printf("Enter your science marks: ");
+    
+    scanf("%d", &science_marks);
+    
+	
+	
+    if  (maths_marks>=30 && science_marks>=40){
+    printf("You are passed in maths and science .You will recieve a ******gift of DAIRY MILK CHOCLATE worth Rs 45*****");
+    
+    }
+    
+    
+    
+    else if (maths_marks>=30 && science_marks<=39)
+
+		{
+     printf("You are passed in maths and failed in science.You will recieve a $$$$$$gift of DAIRY MILK CHOCLATE worth Rs 15$$$$$");
+    }
+    
+    
+    else if(maths_marks<=29 &&science_marks>=40 )
+	{
+    printf("You are passed in science failed in maths.You will recieve a @@@@@@gift of DAIRY MILK CHOCLATE worth Rs 15@@@@@@@");
+	}
+		
+	else{
+		printf("You are failed do more *HARDWORK* ");
+		
+    }
+    
+    return 0;
+   
+}
+    
+    
+    
+
+    
+
+
+
+
+
---
+++ HelloWorld/hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+	printf("--------------------------------------\n");
+	printf("***********Gift Distributer***********\n");
+	printf("--------------------------------------\n");
+	printf("Enter a number: ");
+    scanf("%d", &a);
+	printf("Entered number is : %d", a);
+    printf("\nhello world");
+
+    return 0;
+}
